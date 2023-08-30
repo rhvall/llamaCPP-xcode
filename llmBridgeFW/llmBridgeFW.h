2c143fc51c023b8d8cf6203bd7d86b3bfa412fa1
gg <gg@mm.co>
2023-08-30 22:27:08 +1200

    Add a "llmBridge.framework target

---
+++ llmBridgeFW/llmBridgeFW.h
@@ -0,0 +1,19 @@
+//
+//  llmBridgeFW.h
+//  QuBot
+//
+//  Created by R on 30/08/23.
+//  License GPLv3
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for llmBridgeFW.
+FOUNDATION_EXPORT double llmBridgeFWVersionNumber;
+
+//! Project version string for llmBridgeFW.
+FOUNDATION_EXPORT const unsigned char llmBridgeFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <llmBridgeFW/PublicHeader.h>
+
+
