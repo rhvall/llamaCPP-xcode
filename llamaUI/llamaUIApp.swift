2dd314f903b34c5bb9ded7e19c61497e982f4fc9
r <r@g.co>
2024-08-19 16:29:46 +1000

    Add a new target to run UI on the iPhone

---
+++ llamaUI/llamaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  llamaUIApp.swift
+//  llamaUI
+//
+//  Created by r on 19/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct llamaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
